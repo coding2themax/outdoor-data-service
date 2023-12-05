6fb3739853c741c26798ce7f826e8a1c63568b0a
max quinones <maximilian.quinones@gmail.com>
2023-12-04 20:04:11 -0700

    init

---
+++ src/main/java/com/coding2themax/outdoor/outdoordataservice/OutdoorDataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.coding2themax.outdoor.outdoordataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OutdoorDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OutdoorDataServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/coding2themax/outdoor/outdoordataservice/OutdoorDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coding2themax.outdoor.outdoordataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OutdoorDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
